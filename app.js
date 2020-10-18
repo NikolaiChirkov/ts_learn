f431445265293363d9f89e1fc6c70e592b4e9bee
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-10-19 02:35:54 +0300

    Function types

--- app.js
+++ app.js
@@ -5,4 +5,9 @@ var printResult = function (num) {
     console.log(num);
 };
 printResult(add(5, 12));
-var someValue;
+var combineValues;
+combineValues = add;
+// combineValues = printResult;
+// combineValues = 5;
+console.log(combineValues(8, 8));
+// let someValue: undefined;
--- app.ts
+++ app.ts
@@ -8,4 +8,12 @@ const printResult = (num:number): void => {
 
 printResult(add(5, 12));
 
-let someValue: undefined;
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+// combineValues = printResult;
+// combineValues = 5;
+
+console.log(combineValues(8, 8));
+
+// let someValue: undefined;
