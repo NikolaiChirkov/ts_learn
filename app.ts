9f34baca644e1168b99fbdfac91211219b8c4dc7
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-10-18 22:09:15 +0300

    Type Assignment and Type Inference

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = "The result is: ";
+const number1: number = 5;
+const number2: number = 2.8;
+const printResult: boolean = true;
+const resultPhrase: string = "The result is: ";
 
 const add = (n1: number, n2: number, showResult: boolean, phrase: string) => {
     const result: number = n1 + n2;
