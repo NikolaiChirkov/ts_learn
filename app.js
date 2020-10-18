3da40598f8cbc48384ce049e0b748f16560c24dc
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-10-19 02:41:14 +0300

    Function types & callbacks

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var add = function (n1, n2) {
 var printResult = function (num) {
     console.log(num);
 };
+var addAndHandle = function (n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+};
 printResult(add(5, 12));
 var combineValues;
 combineValues = add;
@@ -11,3 +15,4 @@ combineValues = add;
 // combineValues = 5;
 console.log(combineValues(8, 8));
 // let someValue: undefined;
+addAndHandle(10, 20, function (result) { return console.log(result); });
