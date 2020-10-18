2668c1ebd80ee2646665d1b1b8160f86a4b2cac8
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-10-18 22:24:37 +0300

    Array types

--- app.ts
+++ app.ts
@@ -1,8 +1,16 @@
 const person: {
     name: string,
-    age: number
+    age: number,
+    hobbies: string[]
 } = {
     name: 'Nikolai',
-    age: 20
+    age: 20,
+    hobbies: ['Sports', 'hobbies']
 }
 
+let favoriteActivities: any[];
+favoriteActivities = ["Sports", 1];
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+}
