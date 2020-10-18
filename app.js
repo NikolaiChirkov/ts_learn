ac02b6000287a301d37a8d575f561343a2d4d8ce
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-10-18 21:35:26 +0300

    Numbers, Strings && Booleans

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var number1 = 5;
 var number2 = 2.8;
-var add = function (n1, n2) {
-    return n1 + n2;
+var printResult = true;
+var resultPhrase = "The result is: ";
+var add = function (n1, n2, showResult, phrase) {
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    return result;
 };
-var result = add(number1, number2);
-console.log(result);
+add(number1, number2, printResult, resultPhrase);
--- app.ts
+++ app.ts
@@ -1,9 +1,16 @@
 const number1 = 5;
 const number2 = 2.8;
+const printResult = true;
+const resultPhrase = "The result is: ";
 
-const add = (n1: number, n2: number) => {
-    return n1 + n2;
+const add = (n1: number, n2: number, showResult: boolean, phrase: string) => {
+    const result: number = n1 + n2;
+
+    if (showResult) {
+        console.log(phrase + result);
+    }
+
+    return result;
 }
 
-const result: number = add(number1, number2);
-console.log(result);
+add(number1, number2, printResult, resultPhrase);
